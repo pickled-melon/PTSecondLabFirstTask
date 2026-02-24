dd20a62f08f4fa0ff7b761610e2d5b70f43a07f4
Daniil Knaub <andy.typing@mail.ru>
2026-02-24 13:08:32 +0800

    Added the Logic class

--- Form1.cs
+++ Form1.cs
@@ -12,4 +12,54 @@ namespace PTSecondLabFirstTask
            
         }
     }
+
+    public class Logic
+    {
+        public static string GetAnswer(int trainArrivalTimeHours, int trainArrivalTimeMinutes, int trainDepartureTimeHours, int trainDepartureTimeMinutes, int passengerArrivalTimeHours, int passengerArrivalTimeMinutes)
+        {
+            bool willTheTrainBeOnThePlatform = willTrainBeOnPlatform(trainArrivalTimeHours, trainArrivalTimeMinutes, trainDepartureTimeHours, trainDepartureTimeMinutes, passengerArrivalTimeHours, passengerArrivalTimeMinutes);
+
+            return willTheTrainBeOnThePlatform ? ("ДА") : ("НЕТ");
+        }
+
+        private static bool willTrainBeOnPlatform(int trainArrivalTimeHours, int trainArrivalTimeMinutes, int trainDepartureTimeHours, int trainDepartureTimeMinutes, int passengerArrivalTimeHours, int passengerArrivalTimeMinutes)
+        {
+            const int numberOfMinutesPerHour = 60;
+
+            bool willTheTrainBeOnThePlatform = false;
+
+            int arrivalTotalMinutes = trainArrivalTimeHours * numberOfMinutesPerHour + trainArrivalTimeMinutes;
+
+            int departureTotalMinutes = trainDepartureTimeHours * numberOfMinutesPerHour + trainDepartureTimeMinutes;
+
+            int passengerTotalMinutes = passengerArrivalTimeHours * numberOfMinutesPerHour + passengerArrivalTimeMinutes;
+
+            // Поезд прибывает и отправляется в один и тот же день
+            if (arrivalTotalMinutes <= departureTotalMinutes)
+            {
+                // Пассажир попал во временные рамки между прибытием и отправлением
+                // Пришёл позже прибытия, но раньше отправленмя
+                if (passengerTotalMinutes >= arrivalTotalMinutes && passengerTotalMinutes < departureTotalMinutes)
+                {
+                    willTheTrainBeOnThePlatform = true;
+                }
+            }
+            // Поезд прибывает сегодня и отправляется завтра
+            else
+            {
+                // Пассажир пришёл позже прибытия, но раньше конца суток
+                if (passengerTotalMinutes >= arrivalTotalMinutes)
+                {
+                    willTheTrainBeOnThePlatform = true;
+                }
+                // Пользователь пришёл позже начала суток, но раньше отправления
+                else if (passengerTotalMinutes < departureTotalMinutes)
+                {
+                    willTheTrainBeOnThePlatform = true;
+                }
+            }
+
+            return willTheTrainBeOnThePlatform;
+        }
+    }
 }
